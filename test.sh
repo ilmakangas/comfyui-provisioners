9d2bd0e84a52bcf3bb42f4a744752366f5065833
Teemu Ilmakangas <teemu@dextrey.me>
2024-08-04 12:03:48 +0300

    dsdsa

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
-
-
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
 # This file will be sourced in init.sh
 
@@ -8,10 +6,6 @@
 
 # Packages are installed after nodes so we can fix them...
 
-if [ -z "${HF_TOKEN}" ]; then
-    echo "HF_TOKEN is not set. Exiting."
-    exit 1
-fi
 
 PYTHON_PACKAGES=(
 )
@@ -66,103 +60,26 @@ declare -A CLIP_VISION_MODELS=(
 ### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
 
 function provisioning_start() {
-    DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
-    DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
-    DISK_GB_ALLOCATED=$(($DISK_GB_AVAILABLE + $DISK_GB_USED))
-    provisioning_print_header
-    provisioning_get_nodes
-    provisioning_install_python_packages
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/ckpt" \
-        "${CHECKPOINT_MODELS[@]}"
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/lora" \
-        "${LORA_MODELS[@]}"
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/controlnet" \
-        "${CONTROLNET_MODELS[@]}"
+
     provisioning_get_models_map \
         "${WORKSPACE}/storage/stable_diffusion/models/ipadapter" \
         IPADAPTER_MODELS
     provisioning_get_models_map \
         "${WORKSPACE}/storage/stable_diffusion/models/clip_vision" \
         CLIP_VISION_MODELS
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/vae" \
-        "${VAE_MODELS[@]}"
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/clip" \
-        "${CLIP_MODELS[@]}"
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/unet" \
-        "${UNET_MODELS[@]}"
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/esrgan" \
-        "${ESRGAN_MODELS[@]}"
+
     provisioning_print_end
 }
 
-function provisioning_get_nodes() {
-    for repo in "${NODES[@]}"; do
-        dir="${repo##*/}"
-        path="/opt/ComfyUI/custom_nodes/${dir}"
-        requirements="${path}/requirements.txt"
-        if [[ -d $path ]]; then
-            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
-                printf "Updating node: %s...\n" "${repo}"
-                ( cd "$path" && git pull )
-                if [[ -e $requirements ]]; then
-                    micromamba -n comfyui run ${PIP_INSTALL} -r "$requirements"
-                fi
-            fi
-        else
-            printf "Downloading node: %s...\n" "${repo}"
-            git clone "${repo}" "${path}" --recursive
-            if [[ -e $requirements ]]; then
-                micromamba -n comfyui run ${PIP_INSTALL} -r "${requirements}"
-            fi
-        fi
-    done
-}
-
-function provisioning_install_python_packages() {
-    if [ ${#PYTHON_PACKAGES[@]} -gt 0 ]; then
-        micromamba -n comfyui run ${PIP_INSTALL} ${PYTHON_PACKAGES[*]}
-    fi
-}
-
-function provisioning_get_models() {
-    if [[ -z $2 ]]; then return 1; fi
-    dir="$1"
-    mkdir -p "$dir"
-    shift
-    if [[ $DISK_GB_ALLOCATED -ge $DISK_GB_REQUIRED ]]; then
-        arr=("$@")
-    else
-        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
-        arr=("$1")
-    fi
-
-    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
-    for url in "${arr[@]}"; do
-        printf "Downloading: %s\n" "${url}"
-        provisioning_download "${url}" "${dir}"
-        printf "\n"
-    done
-}
 
 function provisioning_get_models_map() {
     if [[ -z $2 ]]; then return 1; fi
     dir="$1"
-    mkdir -p "$dir"
-    shift
     local -n arr=$2
-
     printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
     for url in "${!arr[@]}"; do
         fn="${arr[$url]}"
         printf "Downloading: %s as %s\n" "${url}" "${fn}"
-        provisioning_download "${url}" "${dir}/${fn}"
         printf "\n"
     done
 }
@@ -175,11 +92,6 @@ function provisioning_print_header() {
 
 function provisioning_print_end() {
     printf "\nProvisioning complete:  Web UI will start now\n\n"
-}
-
-# Download from $1 URL to $2 file path
-function provisioning_download() {
-    wget --header="Authorization: Bearer $HF_TOKEN" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
 }
 
 provisioning_start
